839a62283220f704bbeb933f0510c4cde45bedbd
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2023-03-08 19:45:36 -0500

    Initial Commit

---
+++ TheCatApi/TheCatApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCatApiApp.swift
+//  TheCatApi
+//
+//  Created by Oscar David Myerston Vega on 8/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TheCatApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
